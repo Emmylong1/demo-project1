62cd4c2e6679f71e5e3f56f7e66351ba278e7e60
Ibok Emmanuel <98855013+Emmylong1@users.noreply.github.com>
2023-07-02 19:17:26 +0000

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,44 +1,38 @@
 
-#Create instance profile role for EC2 instance
+# Create instance profile role for EC2 instance
 resource "aws_iam_role" "IAMInstanceProfileRole" {
-    name =  "IAMInstanceProfileRole"
-    assume_role_policy = jsonencode ({
-    Version: "2012-10-17",
-    Statement: [
-        {
-            Effect: "Allow",
-            Action: [
-                "sts:AssumeRole"
-            ],
-            Principal: {
-                Service: [
-                    "ec2.amazonaws.com"
-                ]
-            }
-        }
-    ]
-    })
+  name = "IAMInstanceProfileRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
 
-#Attach S3FullAccess permission EC2 instance role
+# Attach S3FullAccess permission to EC2 instance role
 resource "aws_iam_role_policy_attachment" "s3fullaccessattach" {
-    role = aws_iam_role.IAMInstanceProfileRole.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role       = aws_iam_role.IAMInstanceProfileRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-#Attach RDSreadonly permission to EC2 instance role
+# Attach RDSreadonly permission to EC2 instance role
 resource "aws_iam_role_policy_attachment" "rdsreadonlyattach" {
-    role = aws_iam_role.IAMInstanceProfileRole.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
+  role       = aws_iam_role.IAMInstanceProfileRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
 }
 
-#Create EC2 instance profile
+# Create EC2 instance profile
 resource "aws_iam_instance_profile" "IAMinstanceprofile" {
-    name = "IAMinstanceprofile"
-    role =  aws_iam_role.IAMInstanceProfileRole.name  
+  name = "IAMinstanceprofile"
+  role = aws_iam_role.IAMInstanceProfileRole.name
 }
-
-
-
-
-
