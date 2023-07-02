6d911896a6dac9daeccf680d6bd8e373bbc05b0c
Ibok Emmanuel <98855013+Emmylong1@users.noreply.github.com>
2023-07-02 22:27:09 +0000

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -19,20 +19,5 @@ resource "aws_iam_role" "IAMInstanceProfileRole" {
 EOF
 }
 
-# Attach S3FullAccess permission to EC2 instance role
-resource "aws_iam_role_policy_attachment" "s3fullaccessattach" {
-  role       = aws_iam_role.IAMInstanceProfileRole.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
 
-# Attach RDSreadonly permission to EC2 instance role
-resource "aws_iam_role_policy_attachment" "rdsreadonlyattach" {
-  role       = aws_iam_role.IAMInstanceProfileRole.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
-}
 
-# Create EC2 instance profile
-resource "aws_iam_instance_profile" "IAMinstanceprofile" {
-  name = "IAMinstanceprofile"
-  role = aws_iam_role.IAMInstanceProfileRole.name
-}
